0afc4794484c70f1d9e677512b7b72a1b89c7875
cyotee <not_cyotee@proton.me>
2026-09-14 18:40:30 -0700

    refactor(frontend): remove redundant DTF application shell

--- frontend/apps/dtf/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const sharedConfig = require('../indexedex/next.config')
-
-module.exports = {
-  ...sharedConfig,
-  env: { ...sharedConfig.env, NEXT_PUBLIC_SITE_DEPLOYMENT: 'dtf' },
-}
